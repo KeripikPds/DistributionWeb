da6649adbbca7e42dbd3e81f03789537ea03e20d
unknown <bntng.gpa@gmail.com>
2026-05-28 15:41:34 +0700

    Membuat repository pertama

---
+++ js_login.js
@@ -0,0 +1,28 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+
+const signupLink = document.querySelector("form.login .signup-link a");
+const loginLink = document.querySelector("form.signup .signup-link a");
+
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+
+signupLink.onclick = (e) => {
+  e.preventDefault();
+  signupBtn.click();
+};
+
+loginLink.onclick = (e) => {
+  e.preventDefault();
+  loginBtn.click();
+};
